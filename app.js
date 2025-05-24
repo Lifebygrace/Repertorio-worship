d550049daa1cdf8d1a0113accb3d808b4798f6eb
Lifebygrace <elianztheprodig@gmail.com>
2025-05-24 01:58:10 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+// Firebase and form handling JS will go here.
+// Note: This will be completed in the next step when connecting Auth + Storage.
+console.log('Firebase and app logic pending in app.js');
